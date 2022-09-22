69ba3a3af8b6af8d7bcd475afa0ee21f9368d7db
Osama Nadeem <osama.nadeem@consoliads.com>
2022-09-22 11:50:13 +0500

    version update 7.2.4.0-v14.1.0

---
+++ Example/build/Pods.build/Debug-iphonesimulator/ConsoliAds-IronSource-Adapter.build/DerivedSources/ConsoliAds_IronSource_Adapter_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ConsoliAds_IronSource_AdapterVersionString[];
+ extern const double ConsoliAds_IronSource_AdapterVersionNumber;
+
+ const unsigned char ConsoliAds_IronSource_AdapterVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ConsoliAds_IronSource_Adapter  PROJECT:Pods-1" "\n";
+ const double ConsoliAds_IronSource_AdapterVersionNumber __attribute__ ((used)) = (double)1.;
